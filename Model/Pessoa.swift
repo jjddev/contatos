26c57900b949ed3e69d13ec5daef600e4c041da6
PUCPR <pucpr@LADMAC0120.local>
2019-03-15 21:09:05 -0300

    tableview inicial

---
+++ Model/Pessoa.swift
@@ -0,0 +1,22 @@
+//
+//  Pessoa.swift
+//  contatos
+//
+//  Created by PUCPR on 15/03/19.
+//  Copyright © 2019 PUCPR. All rights reserved.
+//
+
+import Foundation
+
+
+class Pessoa {
+    var id: Int
+    var nome: String
+    var telefone:[String]
+    
+    init(){
+        id = 0
+        nome = ""
+        telefone = []
+    }
+}
