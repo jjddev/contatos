2cf407847db81a49168eb7ec37daf03f1beb4d63
PUCPR <pucpr@LADMAC0120.local>
2019-03-18 20:32:25 -0300

    Constrains na tela de cadastro

---
+++ ViewModel/ContatoViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  ContatoViewModel.swift
+//  contatos
+//
+//  Created by PUCPR on 18/03/19.
+//  Copyright © 2019 PUCPR. All rights reserved.
+//
+
+import Foundation
+
+class ContatoViewModel {
+    var contatoDAO = ContatoDAO()
+    var contato : Contato?
+    
+    init(){
+        
+    }
+    
+}
